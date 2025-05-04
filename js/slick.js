f8de5ad14fdc45a21b7dc9fc73d2a377d7e76e2a
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-05-04 18:08:49 +0300

    2

--- js/slick.js
+++ js/slick.js
@@ -1,22 +1,5 @@
 $(function () {
-  $(".class-gallery-list").slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: false,
-    centerMode: true,
-    cssEase: "linear",
-    prevArrow: ".class-gallery-list-left",
-    nextArrow: ".class-gallery-list-right",
-  });
-});
-
-$(function () {
-  $(".class-reviews-list").slick({
+  $(".w-d-reviews-list").slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -27,7 +10,23 @@ $(function () {
     dots: true,
     centerMode: true,
     cssEase: "linear",
-    prevArrow: ".class-reviews-list-left",
-    nextArrow: ".class-reviews-list-right",
+    prevArrow: ".w-d-reviews-list-left",
+    nextArrow: ".w-d-reviews-list-right",
+  });
+});
+$(function () {
+  $(".w-d-uniqueness-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".w-d-uniqueness-list-left",
+    nextArrow: ".w-d-uniqueness-list-right",
   });
 });
